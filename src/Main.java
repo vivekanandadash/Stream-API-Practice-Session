e0bd9eeb702aadb7c6613eeb598da104ff6d7be5
Vivekananda <vivekanandadash245@gmail.com>
2026-02-05 16:22:53 +0530

    Add new Functional interface(Function) and use  method like distinct,sorted,map

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -7,7 +8,7 @@ import java.util.stream.Collectors;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        List<Integer> arr = Arrays.asList(10,15,14,20,24,36,42);
+        List<Integer> arr = Arrays.asList(10,15,14,20,24,36,42,20,14);
         List<String> str = Arrays.asList("radha","hari","madhav","ramakant");
 //        Predicate<Integer> i   = x ->x % 2!=0;
 //        boolean test = i.test(10);
@@ -16,9 +17,25 @@ public class Main {
         List<String> newStr = str.stream().filter(x->x.startsWith("m")).collect(Collectors.toList());
         List<String> newStr1 = str.stream().filter(x->x.contains("m")).collect(Collectors.toList());
         List<String> newStr2 = str.stream().filter(x->x.length()>4).collect(Collectors.toList());
+        List<Integer> newArr1 = arr.stream().map(x->x+10).collect(Collectors.toList());
+        List<Integer> newArr2 = arr.stream().filter(x->x%2==0).map(y->y+7).collect(Collectors.toList());
+        List<String> newStr3 = str.stream().map(x->x.toUpperCase()).collect(Collectors.toList());
+        List<String> newStr4 = str.stream().map(x->x.substring(0,1).toUpperCase()+x.substring(1)).collect(Collectors.toList());
+        List<Integer> newArr3 = arr.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        List<Integer> newArr4 = arr.stream().sorted().collect(Collectors.toList());
+        List<Integer> newArr5 = arr.stream().distinct().collect(Collectors.toList());
+        List<Integer> newArr6 = arr.stream().distinct().filter(x->x>20).map(x->x+5).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         System.out.println(newArr);
         System.out.println(newStr);
         System.out.println(newStr1);
         System.out.println(newStr2);
+        System.out.println(newArr1);
+        System.out.println(newArr2);
+        System.out.println(newStr3);
+        System.out.println(newStr4);
+        System.out.println(newArr3);
+        System.out.println(newArr4);
+        System.out.println(newArr5);
+        System.out.println(newArr6);
     }
 }
